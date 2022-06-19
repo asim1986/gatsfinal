f6a2168b8e6f394d65938bbc62131a2e7bf425a0
asim1986 <asimmalik110@gmail.com>
2022-06-20 02:31:53 +0530

    Update about.js

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import HeroSection from '../components/homePage/HeroSection';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
   <>
     <SEO />
-    
+    <HeroSection />
+
     <div className="container">
+
       <h2>Welcome to Bollywood story, your number one source for latest news &amp; updates from Bollywood. We’re dedicated to providing you the very best of true stories from Bollywood, with an emphasis on actors &amp; actoress lifestyle, Latest movies, latest web series, songs.</h2>
+
 
 
 
@@ -18,9 +22,7 @@ const IndexPage = () => (
 
 
 
-
     </div>
   </>
 );
-
 export default IndexPage;
