e0f5fa424f23e943cd09843493350025ead1e1fa
asim1986 <asimmalik110@gmail.com>
2022-06-20 02:05:48 +0530

    Update gatsby-config.js

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -3,7 +3,7 @@ const sanityConfig = require('./sanity-config.js');
 
 module.exports = {
   siteMetadata: {
-    title: `TechHub-blog`,
+    title: `Bollywood Story`,
     siteUrl: `https://techhub-blog.netlify.com`,
     description: `TechHub Blog is a platform for latest technology news and updates.`,
   },
