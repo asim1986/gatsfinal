89c9e1de2860cda185a6e3b656244be84016e8ef
asim1986 <asimmalik110@gmail.com>
2022-06-20 02:21:53 +0530

    Update about.js

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import FeaturedBlogs from '../components/homePage/FeaturedBlogs';
 import HeroSection from '../components/homePage/HeroSection';
-import TopCategories from '../components/homePage/TopCategories';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
