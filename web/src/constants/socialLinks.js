207252e2ed288d8eb09c5c40b5ba93be19402ddf
unknown <asimmalik110@gmail.com>
2022-06-20 00:20:36 +0530

    first commit

---
+++ web/src/constants/socialLinks.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+
+export const socialLinks = [
+  {
+    name: 'youtube',
+    url: 'https://www.youtube.com/',
+    icon: <FaYoutube />,
+  },
+  {
+    name: 'instagram',
+    url: 'https://www.youtube.com/',
+    icon: <FaInstagram />,
+  },
+  {
+    name: 'facebook',
+    url: 'https://www.facebook.com/',
+    icon: <FaFacebook />,
+  },
+  {
+    name: 'twitter',
+    url: 'https://www.twitter.com/',
+    icon: <FaTwitter />,
+  },
+];
